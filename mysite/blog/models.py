2b6234188a3298058282db83d2da169c9e9c45bf
Wei <bin.wei-1@corteva.com>
2020-02-12 15:45:12 -0500

    created post and comment models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from django.utils import TIME_ZONE
+from django.urls import reverse
 # Create your models here.
+
+class Post(models.Mode):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length = 256)
+    text = models.TexField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approve_comments = True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TexField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default = False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+    def __str__(self):
+        return self.text
