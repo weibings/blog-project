5b7476a17d35e0e507dff48404e2188dc2265db6
Wei <bin.wei-1@corteva.com>
2020-02-13 14:33:53 -0500

    fix migrate issue and added base.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -7,6 +7,7 @@ from django.utils import timezone
 from blog.forms import PostForm, CommentForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 # Create your views here.
 
@@ -15,7 +16,6 @@ class AboutView(TemplateView):
 
 class PostListView(ListView):
     model = Post
-
     def get_queryset(self):
         return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
@@ -44,3 +44,38 @@ class DraftListView(LoginRequiredMixin, ListView):
     model = PostForm
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull = True).order_by('create_date')
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/login/', views.login, name='login'),
+    # path('account/logout/', views.logout, name='logout', kwargs={'next_page': '/'})
 ]
